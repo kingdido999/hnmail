6785f0b4ad42f608f5cc977aa9ea57f336b50f6e
kingdido999 <kingdido999@gmail.com>
2018-06-10 13:24:56 -0700

    Add basic form

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const path = require('path')
 const Koa = require('koa')
 const Router = require('koa-router')
 const views = require('koa-views')
-const serve = require('koa-static')
+const koaBody = require('koa-body')
 const app = new Koa()
 
 // setup views, appending .ej
 // when no extname is given to render()
 
 app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }))
+app.use(koaBody())
 
 const router = new Router()
 
 router.get('/', async ctx => {
   await ctx.render('pages/index')
+})
+
+router.post('/subscribe', async ctx => {
+  const { email, topics } = ctx.request.body
+  ctx.redirect('/')
 })
 
 app.use(router.routes())
