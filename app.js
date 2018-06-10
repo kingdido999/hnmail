a30c787703782762c6f9d5358c567edc278663c7
kingdido999 <kingdido999@gmail.com>
2018-06-09 22:15:49 -0700

    Add intro and capture section

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const path = require('path')
 const Koa = require('koa')
 const Router = require('koa-router')
 const views = require('koa-views')
+const serve = require('koa-static')
 const app = new Koa()
 
-// setup views, appending .ejs
+// setup views, appending .ej
 // when no extname is given to render()
 
 app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }))
@@ -12,7 +13,7 @@ app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }))
 const router = new Router()
 
 router.get('/', async ctx => {
-  await ctx.render('index')
+  await ctx.render('pages/index')
 })
 
 app.use(router.routes())
