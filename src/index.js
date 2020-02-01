0f5767a8e8d740625d4435d61449e4289b62d169
Desmond Ding <info@desmonding.me>
2020-02-01 14:57:56 -0800

    Remove error email

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const mongoose = require('mongoose')
 const schedule = require('node-schedule')
 const HackerNewsMailer = require('./services/HackerNewsMailer')
 const route = require('./route')
-const Mailer = require('./services/Mailer')
-const { testEmailAddress } = require('../.env')
 
 mongoose.connect('mongodb://localhost/hnmail', {
   useNewUrlParser: true,
@@ -34,16 +32,6 @@ app.use(router.allowedMethods())
 
 app.listen(PORT)
 console.log(`Listening on port: ${PORT}`)
-
-app.on('error', async err => {
-  console.error(err)
-
-  await Mailer.send({
-    to: testEmailAddress,
-    subject: `HNMail Exception: ${err.name}: ${err.message}`,
-    text: err.stack
-  })
-})
 
 // Every Friday at 8AM
 schedule.scheduleJob('0 15 * * 5', HackerNewsMailer.sendNewsletters)
