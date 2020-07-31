831fbde688ad14ed199ac29cc5e241b04a07c6c6
Pengcheng Ding <info@desmonding.me>
2020-07-30 21:11:59 -0700

    Update logging

--- src/services/HackerNewsCrawler.js
+++ src/services/HackerNewsCrawler.js
@@ -1,42 +1,37 @@
 const HackerNewsSearchAPI = require('./HackerNewsSearchAPI')
 
-const getDateTimestampSinceDays = sinceDays => {
+const getDateTimestampSinceDays = (sinceDays) => {
   return Math.trunc(new Date().setDate(new Date().getDate() - sinceDays) / 1000)
 }
 
 class HackerNewsCrawler {
   static async fetchArticlesByTopics(topics, limit = 7) {
-    console.log('Fetching articles...')
+    console.log('Fetching: %o', topics)
 
     const api = new HackerNewsSearchAPI()
     const sinceDays = getDateTimestampSinceDays(7)
     const numericFilters = `created_at_i>${sinceDays}`
     const tags = 'story'
-
     let results = {}
 
     for (let i = 0; i < topics.length; i++) {
       const topic = topics[i]
-      console.log('Searching topic: %s', topic)
-
       const { data } = await api.search({ query: topic, numericFilters, tags })
       const articles = data.hits
         .filter(
           (item, index, items) =>
-            index === items.findIndex(t => t.title === item.title)
+            index === items.findIndex((t) => t.title === item.title)
         )
-        .map(hit => {
+        .map((hit) => {
           return {
             ...hit,
-            hnUrl: `https://news.ycombinator.com/item?id=${hit.objectID}`
+            hnUrl: `https://news.ycombinator.com/item?id=${hit.objectID}`,
           }
         })
         .slice(0, limit)
 
       results[topic] = articles
     }
-
-    console.log('Fetching done.')
 
     return results
   }
